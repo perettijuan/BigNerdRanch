dd2256b2b5044998358b1329ddb98fb21cfa4d4a
Juan Pablo Peretti <jpperetti@arcba-jpperetti.ml.com>
2016-01-29 17:12:50 -0300

    Understanding storyborads

---
+++ iOS-4th-edition/Chapter1/Quiz_xcode_7/Quiz_xcode_7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz_xcode_7
+//
+//  Created by Juan Pablo Peretti on 29/1/16.
+//  Copyright (c) 2016 Juan Pablo Peretti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS-4th-edition/Chapter1/Quiz_xcode_7/Quiz_xcode_7/ViewController.m
@@ -0,0 +1,66 @@
+//
+//  ViewController.m
+//  Quiz_xcode_7
+//
+//  Created by Juan Pablo Peretti on 29/1/16.
+//  Copyright (c) 2016 Juan Pablo Peretti. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
+@property (nonatomic) int questionIndex;
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
+@end
+
+@implementation ViewController
+
+
+
+
+-(void) awakeFromNib
+{
+    [super awakeFromNib];
+    
+    if(self) {
+        // Create the two arrays filled with questions and answers
+        self.questions = @[@"From what is cognac made?",
+                           @"What is 7 + 7?",
+                           @"What is the capital of Vermont?"];
+        self.answers = @[@"Grapes",
+                         @"14",
+                         @"Montpelier"];
+    }
+}
+
+
+- (IBAction)showQuestion:(id)sender
+{
+    self.questionIndex++;
+    
+    // Am I past the last question?
+    if (self.questionIndex == [self.questions count]) {
+        self.questionIndex = 0;
+    }
+    
+    NSString *question = self.questions[self.questionIndex];
+    
+    self.questionLabel.text = question;
+    self.answerLabel.text = @"???";
+    
+}
+
+- (IBAction)showAnswer:(id)sender
+{
+    NSString *answer = self.answers[self.questionIndex];
+    self.answerLabel.text = answer;
+}
+
+
+@end
